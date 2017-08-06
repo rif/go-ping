2bf06a4fdca1bd125fd9f1206dc552dff1fed690
Joseph Gilbert <joseph.gilbert@o3bnetworks.com>
2017-08-05 21:48:08 -0400

    Fix for select cpu utilization

--- ping.go
+++ ping.go
@@ -332,12 +332,11 @@ func (p *Pinger) run() {
 			if err != nil {
 				fmt.Println("FATAL: ", err.Error())
 			}
-		default:
-			if p.Count > 0 && p.PacketsRecv >= p.Count {
-				close(p.done)
-				wg.Wait()
-				return
-			}
+		}
+		if p.Count > 0 && p.PacketsRecv >= p.Count {
+			close(p.done)
+			wg.Wait()
+			return
 		}
 	}
 }
